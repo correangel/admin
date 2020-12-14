62ecf71406a1ed595ff3528e21935888bb6195ef
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-14 17:18:53 -0500

    correccion

--- include/enviar_email_top.php
+++ include/enviar_email_top.php
@@ -67,7 +67,6 @@ $usuario = $row['usuario'];
     $headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     $headers .= "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "Email Enviado con Exito.";
+    echo mail($to,$subject,$message, $headers)?"Enviado Con Exito":"Error al Enviar";
     }
 ?>
