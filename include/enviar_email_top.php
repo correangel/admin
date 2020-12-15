275fe63c20c1636cb631527268682692e80f0e29
AnyheloxD <aojeda.sullon@gmail.com>
2020-12-15 00:19:11 -0500

    12:19 am

--- include/enviar_email_top.php
+++ include/enviar_email_top.php
@@ -44,7 +44,7 @@ body{
     <table width='100%'>
     <tr width = '100%'>
     <td width='33%'></td>
-    <td width='33%'><img src='img/logo.png' width = '100%' alt=''></td>
+    <td width='33%'><img src='tresolu.com/kineshub/img/logo.png' width = '100%' alt=''></td>
     <td width='33%'></td>
     </tr>
      <tr width = '100%'>
